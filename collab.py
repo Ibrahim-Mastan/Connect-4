b35ff332a3ec66da09ed535253336626dbf08aaa
Ibrahim-Mastan <ibi.mastan@outlook.com>
2023-04-15 11:54:34 +0100

    Initial Commit

---
+++ collab.py
@@ -0,0 +1,17 @@
+def create_grid(rows, cols):
+    grid = []
+    for _ in range(rows):
+        row = [' '] * cols
+        grid.append(row)
+    return grid
+
+def display_grid(grid):
+    print('    '.join(map(str, range(1, len(grid[0]) + 1))))
+    for row in grid:
+        print('│ ' + ' │ '.join(row) + ' │')
+        print('────' * len(row) + '│')
+
+rows = 6
+cols = 7
+grid = create_grid(rows, cols)
+display_grid(grid)
